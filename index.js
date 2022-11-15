6646288585f013bf4b19265652fa0448f0b6afca
Keenen Harral <kharral23@yahoo.com>
2022-11-15 15:22:39 -0800

    commit tree 5255815cd5b68bc017a33733e9e1dade0541e805

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+
+function writeCards(snames, enames) {
+    const newLetter = []
+    for(let i = 0; i < snames.length; i++) {
+        newLetter.push(`Thank you, ${snames[i]}, for the wonderful ${enames} gift!`);
+
+    }
+    return newLetter;
+}
+
+function countDown(nums) {
+    
+    while(nums >= 0) {
+        console.log(nums);
+        nums--;
+    }
+}
